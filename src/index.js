2108781379dd686af523c98fd6fa76eaaa6fa9c7
h-maren <26232423+h-maren@users.noreply.github.com>
2024-07-29 12:19:09 -0400

    Create initial set up files Created skeleton index.html, style.css and webpack.config.js before creating functions/application

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+//import {title,cafeInfo} from './loadmain.js';
+//import {title as menuTitle, menu} from './menu.js';
+//import {title as contactTitle, contactContainer} from './contact.js';
+
+console.log('testing');
